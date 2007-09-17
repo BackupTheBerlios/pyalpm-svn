466a20ae9bdb87292237832a885a797a2888c3d0
ragnarok <ragnarok@f83dc630-2f3a-0410-9da3-be83a87ac266>
2007-09-17 15:03:38 +0000

    fixed setup.py to copile one so

--- trunk/setup.py
+++ trunk/setup.py
@@ -21,52 +21,17 @@ from Pyrex.Distutils import build_ext
 
 setup (
 	name = "pyalpm",
-	version = "0.0.15",
+	version = "0.0.2",
 	author = "Stefano Esposito",
 	author_email = "stefano.esposito87@gmail.com",
 	description="Python bindings for pacman's libalpm",
-	packages=["pyalpm"],
-	ext_package="pyalpm",
 	ext_modules=[
 		Extension(
 						"alpm", 
-						["pyalpm/alpm.pyx"],
-						libraries = ["alpm"]),
-		Extension (
-					"package",
-					["pyalpm/package.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"db",
-					["pyalpm/db.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"group",
-					["pyalpm/group.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"config",
-					["pyalpm/config.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"depend",
-					["pyalpm/depend.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"conflict",
-					["pyalpm/conflict.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"sync",
-					["pyalpm/sync.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"transaction",
-					["pyalpm/transaction.pyx"],
-					libraries=["alpm"]),
-		Extension (
-					"exceptions",
-					["pyalpm/exceptions.pyx"],
+						["pyalpm/alpm.pyx","pyalpm/package.pyx", "pyalpm/db.pyx",
+						 "pyalpm/group.pyx", "pyalpm/config.pyx", "pyalpm/depend.pyx",
+						 "pyalpm/conflict.pyx", "pyalpm/sync.pyx", "pyalpm/transaction.pyx",
+						 "pyalpm/exceptions.pyx"],
 					libraries=["alpm"])
 		],
 	cmdclass = {'build_ext': build_ext},
